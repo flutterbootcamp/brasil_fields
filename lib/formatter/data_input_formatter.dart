7a6ab49263d892b2ee5873bd21fea2531e9243bf
rubensmelo <rubensdemelo@gmail.com>
2019-03-10 05:06:25 -0300

    inclui exemplo modelos

--- lib/formatter/data_input_formatter.dart
+++ lib/formatter/data_input_formatter.dart
@@ -17,16 +17,16 @@ class DataFormatter extends TextInputFormatter {
     final StringBuffer newText = StringBuffer();
 
     if (newTextLength >= 3) {
-      newText.write(newValue.text.substring(0, usedSubstringIndex = 2) + '/');
+      newText.write(newValue.text.substring(0, usedSubstringIndex = 2) + '.');
       if (newValue.selection.end >= 2) selectionIndex++;
     }
-    if (newTextLength >= 5) {
-      newText.write(newValue.text.substring(3, usedSubstringIndex = 5) + '/');
+    if (newTextLength >= 6) {
+      newText.write(newValue.text.substring(2, usedSubstringIndex = 5) + '-');
       if (newValue.selection.end >= 5) selectionIndex++;
     }
-
-    if (newTextLength >= usedSubstringIndex)
+    if (newTextLength >= usedSubstringIndex) {
       newText.write(newValue.text.substring(usedSubstringIndex));
+    }
 
     return TextEditingValue(
       text: newText.toString(),
