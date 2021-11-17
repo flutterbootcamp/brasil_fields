5af8aec2177c07cc038d3f642beb30e75c62f187
rubensdemelo <rubensdemelo@gmail.com>
2021-11-17 18:15:46 -0300

    corrige logica separador

--- lib/src/formatters/adiciona_separador.dart
+++ lib/src/formatters/adiciona_separador.dart
@@ -2,11 +2,11 @@ String adicionarSeparador(String texto) {
   var valorFinal = "";
   var pointCount = 0;
   for (var i = texto.length - 1; i > -1; i--) {
-    pointCount = pointCount + 1;
-    if (pointCount == 4) {
+    if (pointCount == 3) {
       valorFinal = "." + valorFinal;
       pointCount = 0;
     }
+    pointCount = pointCount + 1;
     valorFinal = texto[i] + valorFinal;
   }
 
