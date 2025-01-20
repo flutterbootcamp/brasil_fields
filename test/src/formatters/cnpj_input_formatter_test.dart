bb9d459c43f747ff9f150cf445f8ccbb7ff6a715
Victor Emanuel <victor.emanuel@iteris.com.br>
2025-01-20 08:35:20 -0300

    Splits CNPJ tests in two versions

--- test/src/formatters/cnpj_input_formatter_test.dart
+++ test/src/formatters/cnpj_input_formatter_test.dart
@@ -18,18 +18,8 @@ void main() {
     );
 
     test(
-      'padrao alfanumérico',
-      () => expect(evaluate('', 'AAAAAAAAAAAA99'), 'AA.AAA.AAA/AAAA-99'),
-    );
-
-    test(
       'limite 14 digitos',
       () => expect(evaluate('', '199999999999999'), ''),
-    );
-
-    test(
-      'limite 14 digitos alfanumérico',
-      () => expect(evaluate('', 'ABBBBBBBBBBBB99'), ''),
     );
 
     test('backspace', () {
@@ -48,24 +38,6 @@ void main() {
       expect(evaluate('99.9', '99'), '99');
       expect(evaluate('99', '9'), '9');
       expect(evaluate('9', ''), '');
-    });
-
-    test('backspace alfanumérico', () {
-      expect(
-          evaluate('AA.BBB.CCC/DDDD-99', 'AABBBCCCDDDD9'), 'AA.BBB.CCC/DDDD-9');
-      expect(evaluate('AA.BBB.CCC/DDDD-9', 'AABBBCCCDDDD'), 'AA.BBB.CCC/DDDD');
-      expect(evaluate('AA.BBB.CCC/DDDD', 'AABBBCCCDDD'), 'AA.BBB.CCC/DDD');
-      expect(evaluate('AA.BBB.CCC/DDD', 'AABBBCCCDD'), 'AA.BBB.CCC/DD');
-      expect(evaluate('AA.BBB.CCC/DD', 'AABBBCCCD'), 'AA.BBB.CCC/D');
-      expect(evaluate('AA.BBB.CCC/D', 'AABBBCCC'), 'AA.BBB.CCC');
-      expect(evaluate('AA.BBB.CCC', 'AABBBCC'), 'AA.BBB.CC');
-      expect(evaluate('AA.BBB.CC', 'AABBBC'), 'AA.BBB.C');
-      expect(evaluate('AA.BBB.C', 'AABBB'), 'AA.BBB');
-      expect(evaluate('AA.BBB', 'AABB'), 'AA.BB');
-      expect(evaluate('AA.BB', 'AAB'), 'AA.B');
-      expect(evaluate('AA.B', 'AA'), 'AA');
-      expect(evaluate('AA', 'A'), 'A');
-      expect(evaluate('A', ''), '');
     });
   });
 }
